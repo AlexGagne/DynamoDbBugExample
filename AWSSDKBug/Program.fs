2bb4dd38c0e873dc1b729c81a6df9cec64df9a1a
AlexGagne <alex.gagne@broadsign.com>
2021-07-06 16:10:34 -0400

    Adding the bug

---
+++ AWSSDKBug/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open Amazon.DynamoDBv2
+
+[<EntryPoint>]
+let main _ =
+    let localAmazonDynamoDB = Environment.GetEnvironmentVariable("AWS_DEVELOPMENTURL")
+    let clientConfig = AmazonDynamoDBConfig()
+    clientConfig.ServiceURL <- localAmazonDynamoDB
+    let client = new AmazonDynamoDBClient(clientConfig)
+
+    printfn "Describing table"
+    client.DescribeTableAsync("TestName")
+        |> Async.AwaitTask
+        |> Async.Ignore
+        |> Async.RunSynchronously
+    printfn "Described table"
+    0 // return an integer exit code
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DynamoDbBugExample
 Example of the dynamodb bug I have with the AWS SDK
+
+To run the example, in the AWSSDKBug folder, run:
+
+`docker build -t awsbug:latest .`
+
+`docker compose up`
