3b5ddc7f49f80feaa245e8b2d0d6b22679778014
AlexGagne <alex.gagne@broadsign.com>
2021-07-07 08:50:35 -0400

    Updating bug with ideas from aws sdk team

--- AWSSDKBug/Program.fs
+++ AWSSDKBug/Program.fs
@@ -2,13 +2,20 @@
 
 open System
 open Amazon.DynamoDBv2
+open Amazon.DynamoDBv2.Model
+open Amazon.Runtime
 
 [<EntryPoint>]
 let main _ =
-    let localAmazonDynamoDB = Environment.GetEnvironmentVariable("AWS_DEVELOPMENTURL")
+    let localAmazonDynamoDB = "http://localhost:8000"
+    
+    let accessKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID")
+    let secretAccessKey = Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY")
+    let credentials = BasicAWSCredentials(accessKey, secretAccessKey)
+    
     let clientConfig = AmazonDynamoDBConfig()
     clientConfig.ServiceURL <- localAmazonDynamoDB
-    let client = new AmazonDynamoDBClient(clientConfig)
+    let client = new AmazonDynamoDBClient(credentials, clientConfig)
 
     printfn "Describing table"
     client.DescribeTableAsync("TestName")
